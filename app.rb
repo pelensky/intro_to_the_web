7539aa1833de4f8c1375ad1293f8bbddcdaf420c
Dan Pelensky <dan@pelensky.com>
2016-11-22 17:34:20 +0000

    added params

--- app.rb
+++ app.rb
@@ -1,7 +1,13 @@
 require 'sinatra'
 require 'shotgun'
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
